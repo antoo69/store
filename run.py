0c30136961bd81a33c4cdba1a23aee3c7e05151c
antoo69 <antobarry69@gmail.com>
2025-03-09 11:25:40 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 import asyncio
 import logging
 from bot import dp, bot
-import start  # Pastikan handler `/start` dimuat
+import handlers  # Pastikan handler `/start` dimuat
 
 # Konfigurasi logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-# Masukkan router dari start.py
-dp.include_router(start.router)
 
 async def main():
     logging.info("🚀 Bot sedang berjalan...")
