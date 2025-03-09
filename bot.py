e4b8aebcde1956250691773143f43eeea745361a
antoo69 <antobarry69@gmail.com>
2025-03-09 10:28:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from aiogram import Bot, Dispatcher
 from config import TOKEN
-from database import init_db
+from database import init_db, add_products_from_json
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 init_db()
+add_products_from_json("products.json")
