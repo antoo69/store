255c497c6bcdbd37e2c79df9b8c48c8c579a427e
antoo69 <antobarry69@gmail.com>
2025-03-10 00:21:45 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from pyrogram import Client
-
+from config import *
 app = Client("store_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Konfigurasi logging
