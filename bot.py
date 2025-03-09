3dd49c0f5f88240b16012ff58f31a7fa74cb3085
antoo69 <antobarry69@gmail.com>
2025-03-10 00:00:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-from aiogram import Bot, Dispatcher
-from config import TOKEN
- # Tambahkan ini
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
+# Inisialisasi bot
+app = Client("store_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
