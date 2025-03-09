5c28b0ff0862a66e6dc6308e8c22b4e47693d047
antoo69 <antobarry69@gmail.com>
2025-03-10 00:42:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
-# Inisialisasi bot
+# Inisialisasi bot dengan kredensial
 app = Client("store_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
