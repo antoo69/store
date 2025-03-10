022c0f0d98a7dc38307a64ffe224e8ec11b733d5
antoo69 <antobarry69@gmail.com>
2025-03-10 23:15:32 +0700

    Create catalog.py

---
+++ catalog.py
@@ -0,0 +1,18 @@
+CATALOG = {
+    "items": [
+        {
+            "id": "1",
+            "name": "Product 1",
+            "description": "Description for product 1",
+            "price": 10.99,
+            "stock": 100
+        },
+        {
+            "id": "2", 
+            "name": "Product 2",
+            "description": "Description for product 2",
+            "price": 20.99,
+            "stock": 50
+        }
+    ]
+}
