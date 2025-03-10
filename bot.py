66457ab2990def393bade4114299304e2fdeb9c6
antoo69 <antobarry69@gmail.com>
2025-03-10 22:57:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import TOKEN
 
 # Initialize bot instance
 app = Client(
     name="store_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token=TOKEN
 )
